92ebab0f85a7ede7fccbb39bfc4e4089e973e62b
BakersDozenBagels <Bakersdozenbagelsbergman@gmail.com>
2022-06-11 21:59:20 -0400

    Yeet KeepCoding

---
+++ Assets/CustomAudio.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class CustomAudio : MonoBehaviour
+{
+    private AudioSource _src;
+
+    private void Awake()
+    {
+        _src = GetComponent<AudioSource>();
+        Mute = true;
+        _src.enabled = true;
+        _src.Play();
+    }
+
+    private bool _mute;
+    public bool Mute
+    {
+        get
+        {
+            return _mute;
+        }
+        set
+        {
+            _mute = value;
+            if(_mute)
+                _src.volume = 0f;
+            else
+                _src.volume = Volume;
+        }
+    }
+
+    private float _volume;
+    public float Volume
+    {
+        get
+        {
+            return _volume;
+        }
+        set
+        {
+            _volume = value;
+            if(!Mute)
+                _src.volume = value;
+        }
+    }
+}
